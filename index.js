a4f085d009c1cf9a768b9e8543a8819a21b6e63a
dhowe <dhowe@cims.nyu.edu>
2023-11-18 11:48:19 +0800

    adjust

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-/**
- * A template for generating syntax diagrams html file.
+/*
+ * A script for generating riscript syntax diagrams html file.
  * See: https://github.com/chevrotain/chevrotain/tree/master/diagrams for more details
- *
- * usage:
- * - npm install in the parent directory (parser) to install dependencies
- * - Run this in file in node.js (node gen_diagrams.js)
- * - open the "generated_diagrams.html" that will be created in this folder using
- *   your favorite browser.
  */
 import path, { dirname } from "path";
 import fs from "fs";
@@ -20,10 +14,7 @@ const { Constants, tokens } = getTokens();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // extract the serialized grammar.
-console.log('pre parser');
 const parserInstance = new RiScriptParser(tokens, TextTypes);
-console.log('got parser');
-
 const serializedGrammar = parserInstance.getSerializedGastProductions();
 
 // create the HTML Text
@@ -31,4 +22,6 @@ const htmlText = createSyntaxDiagramsCode(serializedGrammar);
 
 // Write the HTML file to disk
 const outPath = path.resolve(__dirname, "./");
-fs.writeFileSync(outPath + "/generated_diagrams.html", htmlText);
+const outFile = outPath + "/riscript-syntax.html";
+fs.writeFileSync(outFile,  htmlText);
+console.log(`wrote syntax diagram to ${outFile}`);
